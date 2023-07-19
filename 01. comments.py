fdc6778bf4b574129cc703b13d0f2878eaf2ff99
ogstevyn <secondsteveoyebamiji@gmail.com>
2023-07-20 01:47:16 +0200

    comments

---
+++ 01. comments.py
@@ -0,0 +1,5 @@
+note = 'A comment is used iin programming to enable easily readability and understanding of codes. in python you start a comment with the pound sign and one other way like that'
+
+#this is a comment
+
+print(note)
